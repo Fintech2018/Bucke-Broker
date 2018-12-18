fd8547da08199a3ab891024c3d4990fe8f220b69
Fintech2018 <44599649+Fintech2018@users.noreply.github.com>
2018-12-19 02:12:18 +0530

    language detection compilation issue

--- src/main/java/com/bucketbroker/utility/LanguageDetection.java
+++ src/main/java/com/bucketbroker/utility/LanguageDetection.java
@@ -1,27 +1,35 @@
 package com.bucketbroker.utility;
 
-//import org.apache.tika.exception.TikaException;
-//import org.apache.tika.language.LanguageIdentifier;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.language.LanguageIdentifier;
 
 
 public class LanguageDetection {
  
-	/*public  static String languageDetector(String text){
-		  LanguageIdentifier identifier = new LanguageIdentifier(text);
+	
+	public static void main(String[] args) {
+		languageDetector("test me! I am english language, please understand. you are not getting it right");
+	}
+	public  static String languageDetector(String text){
+		  LanguageIdentifier identifier = new LanguageIdentifier("test me! I am english language, please understand. you are not getting it right."
+		  		+ "test me! I am english language, please understand. you are not getting it right"
+				  +" test me! I am english language, please understand. you are not getting it right"
+		  		+"  please understand. you are not getting it right"
+				 +" you are not getting it right");
 	      String language = identifier.getLanguage();
 	      System.out.println("Language of the given content is : " + language);
 	      return language;
 	      
-   }*/
+   }
 	
 	
-	public  static String languageDetector(String text){
+	/*public  static String languageDetector(String text){
 	    if(text.contains("La revisora"))  
 		   return "es";
 	    else
 	       return "en";
 	      
-   }
+   }*/
 	
    //javax.mail.internet.MimeMultipart
 }
