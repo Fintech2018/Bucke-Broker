53b00ae1dff8527bfa19ee12d2ebce24f77b270e
JoydeepDe <jdjoydeepde@gmail.com>
2018-12-17 21:17:46 -0500

    Language Translation

--- src/main/java/com/bucketbroker/utility/Utility.java
+++ src/main/java/com/bucketbroker/utility/Utility.java
@@ -1,11 +1,5 @@
 package com.bucketbroker.utility;
 
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
 public class Utility {
 
@@ -35,8 +29,8 @@ public class Utility {
     public static String  neutralFB ="Reviewer is Oprah Winfrey, date	10/11/2016,	state	Texas, source is neutral_feedback ,"+"\r\n"
     		+ "I was contacted for Fund Administration, associates not have expertise in managing risk and complexity, and don't use latest technolgoy to support clients, i also came to you know that they don't  have  accurate safekeeping and reporting of  funds and asset types, i am not happy on their Offering and services.";
     
-    public static String  headerFB ="Reviewer is Ana Patricia,	Date is 10/01/2016, state is New Jersey, source is submitt_feedback ,"+"\r\n";
-    
+    public static String  headerFB ="Reviewer is Ana Patricia,	Date is 10/01/2016, state is New Jersey, source is submitt_feedback ,"+"\r\n"
+   + "I was contacted for Investment Advisory, associate provided detailed helpful infomration, i am very happy on their provided service, i see they are one of the biggest player in the market and been in business for years, service charges are also minimual, services offered in different channels(online, robo-advisor technology & in person).";
 
 
     
