dcd19a168348f3e0a12d8a71bf607f9c9376203f
JoydeepDe <jdjoydeepde@gmail.com>
2018-12-18 23:14:11 -0500

    Translate

--- src/main/java/com/bucketbroker/utility/LanguageTranslate.java
+++ src/main/java/com/bucketbroker/utility/LanguageTranslate.java
@@ -10,16 +10,13 @@ import com.amazonaws.services.translate.model.TranslateTextResult;
 public class LanguageTranslate {
     private static final String REGION = "us-east-2";
 
-    /*public static void main(String[] args) {
-    	String sourceText= "Reviewer is Jose Robert,        Date        10/15/2017 ,state        New York, source is email ,I was contacted for Custody Service, associate provided detailed helpful information, i am very happy on their provided service, i see they are one of the biggest player in the market and been in business for years, service charges are also minimal, services offered in different channels(online, robo-advisor technology, in person).";
+    public static void main(String[] args) {
+    	String sourceText= "El revisor es Ana Patricia, fecha 15/10/2016 ,Estado de Nueva Jersey, la fuente es el correo electronico ,Me contactaron para la administracion del fondo, el asociado proporciono informacion util detallada, estoy muy contento con el servicio prestado, veo que es uno de los mayores jugadores en el mercado y ha estado en el negocio durante anos, cargos de servicio tambien son mini , los servicios ofrecidos en diferentes canales (en linea, tecnologia robo-consejero, en persona).";
     	translateForSomeOtherLanguageToEnglish(sourceText);
-	}*/
+	}
     
    public static String translateForSomeOtherLanguageToEnglish(String languageWholeTextToTranslate){
-    	
-	   BasicAWSCredentials credentialsProvider = new BasicAWSCredentials("AKIAIIIQFUE6PMCA7PIA", "rSifgZt5DbBtpK5SOmPIM13R6YtF7wRXmrU8+OwU");
-	   
-	   AmazonTranslate translate = AmazonTranslateClient.builder().withCredentials(new AWSStaticCredentialsProvider(credentialsProvider))
+    AmazonTranslate translate = AmazonTranslateClient.builder()
                 .withRegion(REGION)
                 .build();
 
@@ -29,7 +26,12 @@ public class LanguageTranslate {
                 .withTargetLanguageCode("en");
         TranslateTextResult result  = translate.translateText(request);
         String convertedToEnglishText = result.getTranslatedText();
-        String transformedText = convertedToEnglishText.replaceAll("dated", "date").replaceAll("email,", "email ,");
+        String transformedText = convertedToEnglishText.replaceAll("dated", "date").replaceAll("email,", "email ,").replaceAll("State of","State")
+        		.replaceAll("/2016,","/2016 ,")
+        		.replaceAll("/2017,","/2017 ,")
+        		.replaceAll("/2018,","/2018 ,")
+        		.replaceAll("/2016,","/2016 ,")
+        		.replaceAll("the fund management","Fund Administration").replaceAll("fund management","Fund Administration");
         System.out.println(transformedText);
         return transformedText;
 
