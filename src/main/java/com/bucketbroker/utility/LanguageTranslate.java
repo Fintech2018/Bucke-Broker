dc7c17344f17dbff32a11800d93e209c21b1018e
JoydeepDe <jdjoydeepde@gmail.com>
2018-12-19 19:51:06 -0500

    Translate

--- src/main/java/com/bucketbroker/utility/LanguageTranslate.java
+++ src/main/java/com/bucketbroker/utility/LanguageTranslate.java
@@ -1,26 +1,41 @@
 package com.bucketbroker.utility;
 
+import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.translate.AmazonTranslate;
 import com.amazonaws.services.translate.AmazonTranslateClient;
 import com.amazonaws.services.translate.model.TranslateTextRequest;
 import com.amazonaws.services.translate.model.TranslateTextResult;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 
 public class LanguageTranslate {
     private static final String REGION = "us-east-2";
 
-    /*
-    public static void main(String[] args) {
+    
+  /*  public static void main(String[] args) {
     	String sourceText= "El revisor es Ana Patricia, fecha 15/10/2016 ,Estado de Nueva Jersey, la fuente es el correo electronico ,Me contactaron para la administracion del fondo, el asociado proporciono informacion util detallada, estoy muy contento con el servicio prestado, veo que es uno de los mayores jugadores en el mercado y ha estado en el negocio durante anos, cargos de servicio tambien son mini , los servicios ofrecidos en diferentes canales (en linea, tecnologia robo-consejero, en persona).";
     	translateForSomeOtherLanguageToEnglish(sourceText);
 	}
+	
 	*/
+	
     
    public static String translateForSomeOtherLanguageToEnglish(String languageWholeTextToTranslate){
-    AmazonTranslate translate = AmazonTranslateClient.builder()
+    /*AmazonTranslate translate = AmazonTranslateClient.builder()
                 .withRegion(REGION)
-                .build();
+                .build(); */
+    
+    
+    AWSCredentialsProvider awsCreds = DefaultAWSCredentialsProviderChain.getInstance();
+    
+    AmazonTranslate translate = AmazonTranslateClient.builder()
+            .withCredentials(new AWSStaticCredentialsProvider(awsCreds.getCredentials()))
+            .withRegion(REGION)
+            .build();
+
 
         TranslateTextRequest request = new TranslateTextRequest()
                 .withText(languageWholeTextToTranslate)
